e616df78b719a79706dc2dd8699b9b06004382da
Qurat ul ain <quratulain.ali@york.ac.uk>
2021-05-24 13:14:12 +0100

    Add SubEmfModelFactory

---
+++ org.eclipse.epsilon.etl.staticanalyser/src/org/eclipse/epsilon/etl/example/staticanalyser/SubEmfModelFactory.java
@@ -0,0 +1,14 @@
+package org.eclipse.epsilon.etl.example.staticanalyser;
+
+import org.eclipse.epsilon.eol.compile.context.IModelFactory;
+import org.eclipse.epsilon.eol.models.IModel;
+
+public class SubEmfModelFactory implements IModelFactory {
+
+	@Override
+	public IModel createModel(String driver) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
