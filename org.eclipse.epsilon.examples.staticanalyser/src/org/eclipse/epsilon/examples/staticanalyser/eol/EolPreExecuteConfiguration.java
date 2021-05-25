b2e5a0db2f42bd372fef598a114785b1dcec4b58
Sorour <sorour.jahanbin@york.ac.uk>
2021-05-25 12:21:26 +0100

    Fix an issue with context in standalone
    eol

--- org.eclipse.epsilon.examples.staticanalyser/src/org/eclipse/epsilon/examples/staticanalyser/eol/EolPreExecuteConfiguration.java
+++ org.eclipse.epsilon.examples.staticanalyser/src/org/eclipse/epsilon/examples/staticanalyser/eol/EolPreExecuteConfiguration.java
@@ -5,6 +5,7 @@ import org.eclipse.epsilon.eol.IEolModule;
 import org.eclipse.epsilon.eol.dom.ModelDeclaration;
 import org.eclipse.epsilon.eol.launch.EolRunConfiguration;
 import org.eclipse.epsilon.eol.staticanalyser.EolStaticAnalyser;
+import org.eclipse.epsilon.eol.staticanalyser.EolStaticAnalysisContext;
 
 public class EolPreExecuteConfiguration extends EolRunConfiguration {
 	IEolModule module;
@@ -23,10 +24,11 @@ public class EolPreExecuteConfiguration extends EolRunConfiguration {
 		long startTime = System.currentTimeMillis();
 		if (module instanceof EolModule) {
 			EolStaticAnalyser staticAnalyser = new EolStaticAnalyser();
-//			for (ModelDeclaration modelDeclaration : module.getDeclaredModelDeclarations()) {
-//				if (modelDeclaration.getDriverNameExpression().getName().equals("EMF"))
-//					staticAnalyser.getContext().setModelFactory(new SubEmfModelFactory());
-//			}
+		//	staticAnalyser.setContext(new EolStaticAnalysisContext());
+			for (ModelDeclaration modelDeclaration : module.getDeclaredModelDeclarations()) {
+				if (modelDeclaration.getDriverNameExpression().getName().equals("EMF"))
+					staticAnalyser.getContext().setModelFactory(new SubEmfModelFactory());
+			}
 			staticAnalyser.validate(module);
 			long stopTime = System.currentTimeMillis();
 			System.out.println("Static Analysis Took : " + (stopTime - startTime));
