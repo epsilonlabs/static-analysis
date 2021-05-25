eb0057879be71c12d8de12778858bf539ce8dc29
Sorour <sorour.jahanbin@york.ac.uk>
2021-05-25 10:53:49 +0100

    code styling

--- org.eclipse.epsilon.eol.staticanalyser.tests/src/org/eclipse/epsilon/eol/staticanalyser/tests/EolStaticAnalyserTests.java
+++ org.eclipse.epsilon.eol.staticanalyser.tests/src/org/eclipse/epsilon/eol/staticanalyser/tests/EolStaticAnalyserTests.java
@@ -14,7 +14,7 @@ import org.eclipse.epsilon.eol.types.EolType;
 import org.junit.Test;
 
 public class EolStaticAnalyserTests {
-	
+
 	@Test
 	public void testPrimitiveTypesVariableDeclaration() throws Exception {
 		assertValid("var i : Integer; (/*Integer*/i).println();");
@@ -22,7 +22,7 @@ public class EolStaticAnalyserTests {
 		assertValid("var s : String; (/*String*/s).println();");
 		assertValid("var r : Real; (/*Real*/r).println();");
 	}
-	
+
 	@Test
 	public void testPrimitiveTypesAssignmentExpression() throws Exception {
 		StringBuffer st = new StringBuffer();
@@ -33,7 +33,7 @@ public class EolStaticAnalyserTests {
 		st.append("(/*Any*/ a) = (/*String*/s);");
 		assertValid(st.toString());
 	}
-	
+
 	@Test
 	public void testCollectionTypesAssignmentExpression() throws Exception {
 		StringBuffer st = new StringBuffer();
@@ -43,6 +43,7 @@ public class EolStaticAnalyserTests {
 		st.append("(/*Bag<Integer>*/bg) = (/*Collection<Integer>*/col);\n");
 		assertValid(st.toString());
 	}
+
 	@Test
 	public void testPrimitiveTypesAssignmentExpressionErrorMessage() throws Exception {
 		StringBuffer st = new StringBuffer();
@@ -51,6 +52,7 @@ public class EolStaticAnalyserTests {
 		st.append("(/*String cannot be assigned to Integer*/i) = s;\n");
 		assertValidMessage(st.toString());
 	}
+
 	public void assertValid(String eol) throws Exception {
 		EolModule module = new EolModule();
 		module.parse(eol);
@@ -58,23 +60,25 @@ public class EolStaticAnalyserTests {
 		staticAnalyser.validate(module);
 		visit(module.getChildren());
 	}
+
 	public void assertValidMessage(String eol) throws Exception {
 		EolModule module = new EolModule();
 		module.parse(eol);
 		EolStaticAnalyser staticAnalyser = new EolStaticAnalyser();
-		List<ModuleMarker> errors =  staticAnalyser.validate(module);
-		visitMarker(module.getChildren(),errors);
+		List<ModuleMarker> errors = staticAnalyser.validate(module);
+		visitMarker(module.getChildren(), errors);
 	}
+
 	protected void visit(List<ModuleElement> elements) {
 		for (ModuleElement element : elements) {
 			if (!element.getComments().isEmpty()) {
-				assertEquals(element.getComments().get(0).toString(),
-						getResolvedType(element).toString());
+				assertEquals(element.getComments().get(0).toString(), getResolvedType(element).toString());
 			}
 			visit(element.getChildren());
 		}
 	}
-	protected void visitMarker(List<ModuleElement> elements, List<ModuleMarker> errors ) {
+
+	protected void visitMarker(List<ModuleElement> elements, List<ModuleMarker> errors) {
 		for (ModuleElement element : elements) {
 			if (!element.getComments().isEmpty()) {
 				int markerLine;
@@ -82,14 +86,14 @@ public class EolStaticAnalyserTests {
 				for (ModuleMarker m : errors) {
 					markerLine = m.getRegion().getStart().getLine();
 					if (commentLine == markerLine)
-						assertEquals(element.getComments().get(0).toString(),
-								m.getMessage());
+						assertEquals(element.getComments().get(0).toString(), m.getMessage());
 				}
-				
+
 			}
-			visitMarker(element.getChildren(),errors);
+			visitMarker(element.getChildren(), errors);
 		}
 	}
+
 	protected EolType getResolvedType(ModuleElement element) {
 		return (EolType) element.getData().get("resolvedType");
 	}
