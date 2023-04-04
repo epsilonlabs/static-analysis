9984506955ebe0037fc19ef1d6ef59c52d869b6f
quratulain-ali <quratulain.ali20@gmail.com>
2023-04-04 17:48:19 +0100

    Check if operations is null before using it

--- org.eclipse.epsilon.eol.staticanalyser/src/org/eclipse/epsilon/eol/staticanalyser/EolStaticAnalyser.java
+++ org.eclipse.epsilon.eol.staticanalyser/src/org/eclipse/epsilon/eol/staticanalyser/EolStaticAnalyser.java
@@ -1356,6 +1356,8 @@ public class EolStaticAnalyser implements IModuleValidator, IEolVisitor {
 
 	public Operation getExactMatchedOperation(OperationCallExpression oc) {
 		List<Operation> operations = matchedOperations.get(oc);
+		if (operations == null)
+			return null;
 		if (operations.isEmpty())
 			return null;
 		if (operations.size() > 1) {
